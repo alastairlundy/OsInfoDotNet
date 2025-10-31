03cf8289a2ce2d9a5609e32a4ddda65b24f677e4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-31 17:51:15 +0000

    Create WindowsPlatformIsVersionExtensions.cs

---
+++ src/OsInfoDotNet.Windows/Extensions/WindowsPlatformIsVersionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using OsInfoDotNet.Abstractions;
+
+namespace AlastairLundy.OsInfoDotNet.Windows.Extensions;
+
+public static class WindowsPlatformIsVersionExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="platform"></param>
+    /// <returns></returns>
+    public static bool IsWindows10(this OperatingSystemInfo platform)
+    {
+        return platform.OSVersion >= new Version(10, 0, 10240)
+               && platform.OSVersion < new Version(10, 0, 22000);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="platform"></param>
+    /// <returns></returns>
+    public static bool IsWindows11(this OperatingSystemInfo platform)
+    {
+        return platform.OSVersion >= new Version(10, 0, 22000)
+               && platform.OSVersion < new Version(10, 0, 29000);
+    }
+}
